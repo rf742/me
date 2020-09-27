1b7567c8b6a55e04206cfc6bcb248ff831bdeaf0
RF <56006623+rf742@users.noreply.github.com>
2020-09-26 20:16:12 -0400

    complete redesign

---
+++ links.js
@@ -0,0 +1,3 @@
+document.write('<link rel="stylesheet" href="https://fonts.googleapis\
+.com/css?family=EB+Garamond">\
+<link rel="stylesheet" href="style.css">');
---
+++ nav.js
@@ -0,0 +1,40 @@
+document.write('<nav class="navbar">\
+	<ul class="navbar-nav">\
+    	<li class="nav-item">\
+          <a href="about.html" class="nav-link">\
+            <img src="me.svg" width="5rem"/>\
+            <span class="link-text">about me</span>\
+          </a>\
+        </li>\
+    	<li class="nav-item">\
+          <a href="groff.html" class="nav-link">\
+            <img src="groff.svg" width="5rem"/>\
+            <span class="link-text">groff</span>\
+          </a>\
+        </li>\
+    	<li class="nav-item">\
+          <a href="phys.html" class="nav-link">\
+            <img src="physics.svg" width="5rem"/>\
+            <span class="link-text">physics</span>\
+          </a>\
+        </li>\
+    	<li class="nav-item">\
+          <a href="chem.html" class="nav-link">\
+            <img src="flask.svg" width="5rem"/>\
+            <span class="link-text">chemistry</span>\
+          </a>\
+        </li>\
+    	<li class="nav-item">\
+          <a href="bio.html" class="nav-link">\
+            <img src="microscope.svg" width="5rem"/>\
+            <span class="link-text">biology</span>\
+          </a>\
+        </li>\
+    	<li class="nav-item">\
+          <a href="index.html" class="nav-link">\
+            <img src="home.svg" width="5rem"/>\
+            <span class="link-text">home</span>\
+          </a>\
+        </li>\
+    </ul>\
+  </nav>');
