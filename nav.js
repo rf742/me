31cb3a166d275597c4c38763d10e943191afe5d3
rf <56006623+rf742@users.noreply.github.com>
2022-03-13 23:03:41 -0400

    simplified navigation

--- nav.js
+++ nav.js
@@ -6,34 +6,16 @@ document.write('<nav class="navbar">\
             <span class="link-text">about me</span>\
           </a>\
         </li>\
-    	<li class="nav-item">\
-          <a href="groff.html" class="nav-link">\
-            <img src="groff.svg" width="5rem"/>\
-            <span class="link-text">groff</span>\
-          </a>\
-        </li>\
       <li class="nav-item">\
         <a href="cs.html" class="nav-link">\
           <img src="coding.svg" width="5rem"/>\
-          <span class="link-text">groff</span>\
+          <span class="link-text">computers</span>\
         </a>\
       </li>\
     	<li class="nav-item">\
-          <a href="phys.html" class="nav-link">\
-            <img src="physics.svg" width="5rem"/>\
-            <span class="link-text">physics</span>\
-          </a>\
-        </li>\
-    	<li class="nav-item">\
-          <a href="chem.html" class="nav-link">\
+          <a href="sci.html" class="nav-link">\
             <img src="flask.svg" width="5rem"/>\
-            <span class="link-text">chemistry</span>\
-          </a>\
-        </li>\
-    	<li class="nav-item">\
-          <a href="bio.html" class="nav-link">\
-            <img src="microscope.svg" width="5rem"/>\
-            <span class="link-text">biology</span>\
+            <span class="link-text">science</span>\
           </a>\
         </li>\
     	<li class="nav-item">\
